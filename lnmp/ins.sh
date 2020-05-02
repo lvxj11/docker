95c79cb520c5d0c24586e93212a71bb1c993c515
lvxj11 <lvxj11@126.com>
2020-05-02 09:42:37 +0800

    2020.05.02同步仓库

---
+++ lnmp/ins.sh
@@ -0,0 +1,1 @@
+wget http://soft.vpser.net/lnmp/lnmp1.6.tar.gz -cO lnmp1.6.tar.gz && tar zxf lnmp1.6.tar.gz && cd lnmp1.6 && LNMP_Auto="y" DBSelect="3" DB_Root_Password="35750129" InstallInnodb="y" PHPSelect="5" SelectMalloc="1" CheckMirror="n" ./install.sh lnmp
