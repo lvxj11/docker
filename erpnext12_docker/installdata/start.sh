c3785c24a541617e284dd0a82380e08bf489656c
lvxj11 <lvxj11@163.com>
2020-12-13 13:48:58 +0800

    erpnext12修正镜像源

--- erpnext12_docker/installdata/start.sh
+++ erpnext12_docker/installdata/start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-source /etc/profile
 sudo service redis-server restart
 sudo service mysql restart
-export PATH=/home/frappe/.local/bin:\$PATH
+export PATH=/home/frappe/.local/bin:$PATH
 export LC_ALL=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
 export LANG=en_US.UTF-8
