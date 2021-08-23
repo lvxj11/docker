16b98aff8d7cc3d4bc0c9ac2d328694d6c7cdd81
root <root@localhost.localdomain>
2021-08-23 20:32:08 +0800

    添加vuecil与golang开发环境集合容器

--- ubuntu18mr/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker run -itd \
-  -v MR_tmp:/tmp \
-  -v MR_home:/home \
-  --name ubuntu18mr ubuntu18mr:t2
---
+++ vuecliGolang/installdata/installapp.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+cd ~
+# 更新系统
+echo "===================更新系统==================="
+apt update
+apt upgrade -y
+# 更新npm
+echo "===================更新npm==================="
+npm install -g npm
+# 安装vuecil
+echo "===================安装vuecil==================="
+npm install -g @vue/cli
+# 测试安装
+echo "===================测试安装是否成功，如显示版本号为成功，否则安装失败。==================="
+vue --version
+# 安装golang
+echo "===================安装golang==================="
+wget -O go.tar.gz https://golang.google.cn/dl/go1.17.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go.tar.gz
+echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
+export PATH=$PATH:/usr/local/go/bin
+# 测试安装
+echo "===================测试安装是否成功，如显示版本号为成功，否则安装失败。==================="
+go version
+# 建立工作目录
+echo "===================建立工作目录==================="
+mkdir /myProject
+# 清理垃圾,ERPNext安装完毕
+echo "===================清理垃圾,ERPNext安装完毕==================="
+sudo -H apt clean
+sudo -H apt autoremove
+sudo -H rm -rf /var/lib/apt/lists/*
+sudo -H npm cache clean --force
+sudo -H yarn cache clean
+exit 0
