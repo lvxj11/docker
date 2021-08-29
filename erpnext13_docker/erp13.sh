5a75a41aa933faffbd0aaae26df4d0dd10cb1b21
lvxj11 <lvxj11@126.com>
2021-08-29 15:34:13 +0800

    erpnext13基础镜像更换为ubuntu20.04版本，优化安装脚本。

--- erpnext13_docker/erp13.sh
+++ erpnext13_docker/erp13.sh
@@ -3,4 +3,4 @@ C_NAME="ERPnext13"
 docker run -itd -p 46114:8000 -p 46115:9000 \
   -v ${C_NAME}_db:/var/lib/mysql \
   -v ${C_NAME}_sites:/home/frappe/frappe-bench/sites \
-  --name ${C_NAME} registry.cn-beijing.aliyuncs.com/lvxj11/erpnext13:2021.04.05
+  --name ${C_NAME} registry.cn-beijing.aliyuncs.com/lvxj11/erpnext13:latest
--- erpnext13_docker/installdata/tt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$(echo $* |grep -o fromGitee)" == "fromGitee" ];then
-    echo "有"
-else
-    echo "无"
-fi
