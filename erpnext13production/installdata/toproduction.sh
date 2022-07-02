24db04dd53cb4951e5e434636b477ad903bde904
lvxj11 <35598180+lvxj11@users.noreply.github.com>
2022-07-02 15:37:11 +0800

    Delete erpnext13production/installdata directory

--- erpnext13production/installdata/toproduction.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# v0.1 2022.07.1
-set -e
-sudo apt update
-DEBIAN_FRONTEND=noninteractive sudo apt upgrade -y
-cd ~/frappe-bench
-sudo bench setup production frappe --yes
-bench doctor
-bench version
