9bb3e24a04cdf59bc1927720479ff495bfb904ae
lvxj11 <35598180+lvxj11@users.noreply.github.com>
2022-07-02 15:05:11 +0800

    Update toproduction.sh

--- erpnext13production/installdata/toproduction.sh
+++ erpnext13production/installdata/toproduction.sh
@@ -3,8 +3,6 @@
 set -e
 sudo apt update
 DEBIAN_FRONTEND=noninteractive sudo apt upgrade -y
-DEBIAN_FRONTEND=noninteractive sudo apt install -y supervisor
-sudo supervisor -c "/etc/supervisor/supervisord.conf"
 cd ~/frappe-bench
 sudo bench setup production frappe --yes
 bench doctor
