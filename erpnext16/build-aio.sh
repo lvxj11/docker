e1b85ff0bc7b0b77a4dfcc54e2774167b7f87de8
lvxj11 <lvxj11@126.com>
2026-01-29 05:16:44 -0500

    重构dockerfile，精简镜像

---
+++ erpnext16/build-aio.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 获取apps.json的base64
+export APPS_JSON_BASE64=$(base64 -w 0 ./aiofile/apps.json)
+
+case $1 in
+    "test")
+        docker build \
+            --build-arg=APPS_JSON_BASE64=${APPS_JSON_BASE64} \
+            --target=test \
+            --progress=plain \
+            --file=Containerfile-aio .
+        ;;
+    *)
+        docker build \
+            --build-arg=APPS_JSON_BASE64=${APPS_JSON_BASE64} \
+            --tag=lvxj11/erpnext:v16 \
+            --target=erpnext_aio \
+            --progress=plain \
+            --file=Containerfile-aio .
+        ;;
+esac
