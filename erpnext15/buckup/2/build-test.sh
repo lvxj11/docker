56a314e2969d197ecee772fc3be4bbe505a8f480
lvxj11 <lvxj11@126.com>
2026-02-26 14:04:12 +0800

    refactor: 清理旧版本备份文件及更新依赖版本
    删除erpnext15和erpnext16的旧备份目录及文件
    更新nvm版本至v0.40.3
    更新Node.js和Python版本
    优化Dockerfile配置

--- erpnext15/buckup/2/build-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-export DOCKER_BUILDKIT=1
-# docker build --progress=plain --no-cache -t lvxj11/erpnext:v15 .
-docker buildx build --progress=plain --target test .
-set +ex
-exit 0
--- erpnext16/backup/2/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-export DOCKER_BUILDKIT=1
-# docker build --progress=plain --no-cache -t lvxj11/erpnext:v16 .
-docker buildx build --progress=plain -t lvxj11/erpnext:v16 --target erpnext .
-set +ex
-exit 0
